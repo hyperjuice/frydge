b9d010518706c207a16c0d9f144000b30d326d25
Darrell Cohn <darrellcohn@gmail.com>
2015-04-16 10:07:42 -0700

    adding production db

--- models/index.js
+++ models/index.js
@@ -6,6 +6,19 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
+
+// NEEDED FOR HEROKU ///////////
+if(config.use_env_variable){
+  var db_info = process.env[config.use_env_variable].match(/([^:]+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+  config.dialect=db_info[1];
+  config.username=db_info[2];
+  config.password=db_info[3];
+  config.host=db_info[4];  
+  config.port=db_info[5];  
+  config.database=db_info[6];  
+}
+//////////////////////////////
+
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
